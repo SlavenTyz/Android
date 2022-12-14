692cca036b7e6f34fb8929a15ad321f118299ce2
Вячеслав <79139467294@yandex.ru>
2022-12-14 13:46:20 +0700

    4 homework navigetion

--- Project/Fragment2/app/src/main/java/com/example/fragment2/SecondFragment.kt
+++ Project/Fragment2/app/src/main/java/com/example/fragment2/SecondFragment.kt
@@ -4,22 +4,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
+    lateinit var controller: NavController
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        controller=findNavController()
         val button = view.findViewById<Button>(R.id.switchFirstButton)
         button.setOnClickListener {
-            (requireActivity() as MainActivity).switchToFirstFragment()
+            controller.navigateUp()
+
         }
     }
 
-
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            SecondFragment()
-    }
 }
